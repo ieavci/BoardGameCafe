5d89c6e24a4841275c76f1404d694c647430e86a
ieavci <iavcci123@gmail.com>
2024-05-11 12:37:28 +0300

    11.05  GameOverviewScreen Tasarımı Yapıldı

--- components/GameItem.js
+++ components/GameItem.js
@@ -1,37 +1,71 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 const GameItem = ({ id, name, bggUrl, rank, year, minPlayers, maxPlayers, minTime, maxTime, avgTime }) => {
   const navigation = useNavigation();
 
   const navigateToGameDetail = () => {
-    navigation.navigate('GameDetailScreen', { gameId: id, name:name }); // Oyun detay sayfasına giderek gameId parametresini iletiyoruz
+    navigation.navigate('GameDetailScreen', { gameId: id, name: name }); // Oyun detay sayfasına giderek gameId parametresini iletiyoruz
   };
 
   return (
-    <TouchableOpacity onPress={navigateToGameDetail}>
+    <Pressable onPress={navigateToGameDetail} style={({ pressed }) => pressed ? styles.pressed : null}>
       <View style={styles.gameItem}>
-        <Text style={styles.gameName}>{name}</Text>        
-        <Text>Yıl: {year}</Text>
-        <Text>Oyuncu: {minPlayers}-{maxPlayers}</Text>        
-        <Text>Oyun Süresi: {avgTime} dk.</Text>
+        <View style={styles.gameItemIngredients}>
+          <Text style={styles.gameName}>{name}</Text>
+          <Text>Yıl: {year}</Text>
+          <Text>Oyuncu: {minPlayers}-{maxPlayers}</Text>
+          <Text>Oyun Süresi: {avgTime} dk.</Text>
+        </View>
+        <View style={styles.detailContainer}>
+          <MaterialIcons style={styles.go} name="arrow-forward-ios" size={30} color="#ff7a07" />
+        </View>
       </View>
-    </TouchableOpacity>
+    </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
+
+  pressed: {
+    opacity: 0.7,
+
+  },
   gameItem: {
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
-    backgroundColor: 'orange',
+    marginHorizontal: 15,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    elevation: 4,
+    shadowColor: '#171717',
+    shadowOffset: {
+      width: -2,
+      height: 4,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
     borderRadius: 10,
+
+  },
+  gameItemIngredients: {
+    flex: 6,
+    marginHorizontal:10
+  },
+  detailContainer: {
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    flex: 1
   },
   gameName: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 21,
+    fontWeight: 'normal',
+    marginHorizontal: -5,
+    marginVertical: 3,
+    color: '#ff7a07'
   },
 });
 
